c0e29db70292bbcbaf6df9f06c66440ee0d95a8d
Venukoratla <koratlavenu09@gmail.com>
2023-02-07 14:47:33 +0000

    first reactjs

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className="bg-color">
+    <h1 className="heading">Congratulations</h1>
+    <div className="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>Anubose inistitute of technology at bhadrachalm road</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
